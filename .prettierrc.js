708ab984212efa7744df20c61e37483ee4c094a1
djzhang <trujunzhang@gmail.com>
2023-09-06 19:59:32 +0800

    init project

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  printWidth: 120,
+  jsxBracketSameLine: false,
+  useTabs: false,
+  eslintIntegration: false,
+  tslintIntegration: true,
+  parser: 'typescript',
+  requireConfig: false,
+  stylelintIntegration: false,
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: false
+};
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // preset: 'react-native',
+  // setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['./jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation|@react-native)',
+  ],
+}
