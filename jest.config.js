2d4efb99a4f37601cf52348d887c85c5a1000998
djzhang <trujunzhang@gmail.com>
2023-09-09 19:54:56 +0800

    add tailwind helper

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  // preset: 'react-native',
-  // setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['./jest.setup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation|@react-native)',
   ],
