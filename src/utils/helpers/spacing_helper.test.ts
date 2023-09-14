b04ab1c63277c5de8106d6777514a07021f8fcf1
djzhang <trujunzhang@gmail.com>
2023-09-14 09:33:39 +0800

    v1.0.4

--- src/utils/helpers/spacing_helper.test.ts
+++ src/utils/helpers/spacing_helper.test.ts
@@ -14,5 +14,6 @@ describe("SpacingHelper", () => {
     expect(new SpacingHelper().fix("lg:max-w-[80rem] lg:mt-[24px]")).toBe('lg:max-w-[80rem] lg:mt-6');
     expect(new SpacingHelper().fix("lg:max-w-[80rem] xl:mx-[24px]")).toBe('lg:max-w-[80rem] xl:mx-6');
     expect(new SpacingHelper().fix("lg:max-w-[80rem] m-[24px]")).toBe('lg:max-w-[80rem] m-6');
+expect(new SpacingHelper().fix("lg:max-w-[80rem] sm:m-[24px] m-[48px] lg:m-[48px]")).toBe('lg:max-w-[80rem] sm:m-6 m-12 lg:m-12');
   });
 });
